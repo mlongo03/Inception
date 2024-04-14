56f73e25e8949e85c806bf4a1be414cbe1d13710
Manuele Longo <manuele.lng03@gmail.com>
2024-04-14 16:14:34 +0200

    wordpress fully through wp-cli

--- srcs/requirements/wordpress/tools/wordpress.sh
+++ srcs/requirements/wordpress/tools/wordpress.sh
@@ -6,24 +6,15 @@ cd /var/www/
 
 wp core download --version=6.5 --allow-root
 
-mv /var/www/wp-config-sample.php /var/www/wp-config.php
-
-mv /wp-config.php /var/www/wp-config.php
-
-sed -i -r "s/db1/$db_name/1"   wp-config.php
-sed -i -r "s/user/$db_user/1"  wp-config.php
-sed -i -r "s/pwd/$db_pwd/1"    wp-config.php
-
-# wp config create	--allow-root \
-# 					--dbname=$db_name \
-# 					--dbuser=$db_user \
-# 					--dbpass=$db_pwd \
-# 					--dbhost=mariadb:3306 --path='/var/www'
+wp config create	--allow-root \
+					--dbname=$db_name \
+					--dbuser=$db_user \
+					--dbpass=$db_pwd \
+					--dbhost=mariadb:3306 --path='/var/www'
 
 wp core install --url=$DOMAIN_NAME/ --title=$WP_TITLE --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
 
 wp user create $WP_USR $WP_EMAIL --role=author --user_pass=$WP_PWD --allow-root
-
 
 mkdir /run/php
 
