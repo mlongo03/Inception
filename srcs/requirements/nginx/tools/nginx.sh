4162416bd2d5e4b78ff85f3d50dee67c76bbe9f1
Manuele Longo <mlongo@e4r8p3.42roma.it>
2024-04-15 14:43:20 +0200

    adjusting makefile

--- srcs/requirements/nginx/tools/nginx.sh
+++ srcs/requirements/nginx/tools/nginx.sh
@@ -3,10 +3,10 @@
 openssl req -x509 -nodes -out $CERTS_ -keyout $KEYS_ -subj "/C=FR/ST=IDF/L=Paris/O=42/OU=42/CN=$DOMAIN_NAME/UID=mlongo"
 
 # l'espansione aggiunge /
-sed -i -r "s/first/$CERTS_2/g"    /etc/nginx/nginx.conf
+sed -i -r "s#first#$CERTS_#g"    /etc/nginx/nginx.conf
 
-sed -i -r "s/second/$KEYS_2/g"    /etc/nginx/nginx.conf
+sed -i -r "s#second#$KEYS_#g"    /etc/nginx/nginx.conf
 
-sed -i -r "s/third/$DOMAIN_NAME/g"    /etc/nginx/nginx.conf
+sed -i -r "s#third#$DOMAIN_NAME#g"    /etc/nginx/nginx.conf
 
 nginx -g "daemon off;"
